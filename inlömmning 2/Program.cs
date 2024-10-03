db6dbfd3e419a8489a86c6adc3702592aea85d8c
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-10-03 14:26:19 +0200

    Add project files.

---
+++ inlömmning 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin");
+            int höjdelin=int.Parse(Console.ReadLine());
+            Console.WriteLine("hur långt hoppa alma");
+            int höjdalma=int.Parse(Console.ReadLine());
+            int sum = höjdelin - höjdalma;
+            Console.WriteLine(sum);
+            
+         
+            
+            
+            
+            
+            
+        }
+    }
+}
